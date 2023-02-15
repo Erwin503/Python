a1224658b9e7a1fe21a5cc6de3da715756a4ee6d
Ervin A <chelovek.ap@gmail.com>
2023-02-15 10:15:50 +0300

    task3

--- task3.py
+++ task3.py
@@ -1,2 +1,16 @@
-str = input('Введите задачу в формате "первое число" "оператор" "второе число"')
-arr = str.split(' ')
+while True:
+    str = input('Введите задачу в формате "первое число" "оператор" "второе число": :')
+    arr = str.split(' ')
+    num1 = int(arr[0])
+    num2 = int(arr[2])
+    char = arr[1]
+    if char == '+':
+        print(num1 + num2)
+    elif char == '-':
+        print(num1 - num2)
+    elif char == '*':
+        print(num1 * num2)
+    elif char == '/' and num2 != 0:
+        print(num1 / num2)
+    else:
+        print('Error: Invalid character')
