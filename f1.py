4f26680145c7d808b10bf4d29f523b8c30a53757
Ervin A <chelovek.ap@gmail.com>
2023-02-28 20:35:41 +0300

    HELP

--- f1.py
+++ f1.py
@@ -1,6 +1,61 @@
-i = 7
-a = 5
-while i < 11:
-    a += i
-    i += 2
-print(a)
+
+def find(i, str1):
+    num1 = ''
+    num2 = ''
+    start = 0
+    end = 0
+    operators = ['+', '-', '*', '%', '/']
+    for j in range(i - 1, -1, -1):
+        if str1[j] not in operators:
+            num1 = num1 + str1[j]
+            start = j
+        else:
+            break
+    for j in range(i + 1, len(str1)):
+        if str1[j] not in operators:
+            num2 = num2 + str1[j]
+            end = j
+        else:
+            break
+    num1 = num1[::-1]
+
+    if str1[i] == '+':
+        str1 = str1[:start:] + str(int(num1) + int(num2)) + str1[end+1:]
+    elif str1[i] == '-':
+        str1 = str1[:start:] + str(int(num1) - int(num2)) + str1[end+1:]
+    elif str1[i] == '*':
+        str1 = str1[:start:] + str(int(num1) * int(num2)) + str1[end+1:]   
+    elif str1[i] == '/':
+        str1 = str1[:start:] + str(int(num1) / int(num2)) + str1[end+1:]    
+    elif str1[i] == '%':
+        str1 = str1[:start:] + str(int(num1) % int(num2)) + str1[end+1:]
+    return[str1, end - 1]
+    
+input()
+str1 = '9+6*6+8'
+HIoperators = ['*', '%', '/']
+LOoperators = ['+', '-']
+isReady = False
+tmp = []
+length = len(str1)
+
+# for i in range(len(str1)):
+#     if str1[i] == ')':
+    #     for j in range(i, 0, -1):
+    #         if str[j] == '()':
+    #             str1 = str1[:j] + calculate(j, str1) + str1[i:]
+for a in range(length):
+    if str1[a] in HIoperators:
+        tmp = find(a, str1)
+        str1 = tmp[0]
+        a = tmp[1]
+        print(str1)
+        length = len(str1) - 1
+for a in range(length):
+    if str1[a] in LOoperators:
+        tmp = find(a, str1)
+        str1 = tmp[0]
+        a = tmp[1]
+        print(str1)
+        length = len(str1) - 1
+print(str1)
--- task4.py
+++ task4.py
@@ -1,3 +1,7 @@
+import sys
+
+sys.setrecursionlimit(2000)
+
 def find(i, str1):
     num1 = ''
     num2 = ''
@@ -33,38 +37,31 @@ def find(i, str1):
 
 
 def calculate(str1):
-    operators = ['+', '-', '*', '%', '/']
-    isReady = True
-    for i in range(len(str1) - 1):
-        if str1[i] in operators:
-            isReady = False
-            break
-    if isReady:
-        return str1
-    else:
-        for i in range(len(str1)):
-            if str1[i] == ')':
-                for j in range(i - 1, 0 , -1):
-                    if str1[j] == '(':
-                        str1 = str1[:j] + calculate(str1[j+1:i])
-        for i in range(len(str1) - 1):
-            if str1[i] == '*' or str1[i] == '/' or str1[i] == '%':
-                    str1 = find(i, str1)
-        for i in range(len(str1)):
-            if str1[i] in operators:
-                str1 = find(i, str1)
-            for i in range(len(str1) - 1):
-                if str1[i] in operators:
-                    isReady = False
-                    break
-                if isReady:
-                    return str1
-                else: 
-                    return calculate(str1)
+    HIoperators = ['*', '%', '/']
+    LOoperators = ['+', '-']
+    isReady = False
     
+    # for i in range(len(str1)):
+    #     if str1[i] == ')':
+        #     for j in range(i, 0, -1):
+        #         if str[j] == '()':
+        #             str1 = str1[:j] + calculate(j, str1) + str1[i:]
+    for a in range(len(str1)):
+
+        if str1[a] in HIoperators:
+            str1 = find(a, str1)
+            print(str1)
+    for a in range(len(str1)):
+
+        if str1[a] in LOoperators:
+            str1 = find(a, str1)
+            print(str1)
+            
+    return str1
 
 while True:
-    str1 = input('Введите задачу: ')
+    # str1 = input('Введите задачу: ')
+    str1 = "9 + 5   *6"
     arr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '+', '-', '*', '%', '/', '(', ')']
     i = 0
     while i < len(str1):
