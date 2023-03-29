af67a9df5d0d9e1aef35b9b336632dd396b798c1
Ervin A <chelovek.ap@gmail.com>
2023-03-29 12:53:09 +0300

    task10

--- task10.py
+++ task10.py
@@ -3,11 +3,15 @@ from tkinter import *
 
 def converter():
     line = txt.get()
-    phone_regex = r"(?:\+7|8)?[- ](?:(?:\d{3}[- ]?\d{3}[- ]?\d{2}[- ]?\d{2}))"
+    phone_regex = r"(?:(?:\+7|8)?[- ]\d{3}[- ]?\d{3}[- ]?\d{2}[- ]?\d{2}|8?[- ]3652[- ]?\d{2}[- ]?\d{2}[- ]?\d{2}|8?[- ]3652[- ]?\d{3}[- ]?\d{3})"
     result = "Результат:"
     tmp = re.findall(phone_regex, line)
     for i in tmp:
-        result = result + ' ' + i
+        if i[0] == '8':
+            i = "+7" + i[1:]
+        i = i.replace(' ', '')
+        i = i.replace('-', '')
+        result = result + '\n' + i
     lbl.configure(text = result)
 
 window = Tk()
@@ -24,4 +28,4 @@ btn = Button(window, text ="Convert", command = converter)
 btn.grid(column = 1, row =0)
 
 window.mainloop()
-# Это мой номер телефона: +7 123 456-78-90. А это номер моего друга: 8-800-123-45-67.
+# Это мой номер телефона: +7 123 456-78-90. А это номер моего друга: 8-800-123-45-67. 8 3652-09-67 80 gnbtgbny 8 3652-567 789
